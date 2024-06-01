2419e75442732879d0c2f1882f08f7cd09a628f2
Dhanyalakshmi K N <dhanyalakshmi.k@priyarajaelectronics.com>
2024-06-01 16:43:07 +0530

    [TASK]: Add helloworld_application.c file Description: Add helloworld_application.c file, which intensionally crashes helloworld application.

---
+++ src/hello_world_application.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    // Print "Hello World" to the console
+    printf("Hello World\n");
+    
+    // Pause execution for 10 seconds
+    sleep(10);
+
+    // Intentionally create a crash in this application
+    
+    int variable = 8;
+    
+    int *ptr = NULL;
+    
+    // Attempt to dereference a NULL pointer and assign the address of 'variable' to it, which will cause a segmentation fault
+    *ptr = &variable;
+    
+    return 0;
+
+}
